d2fa4d21c8ff7a52cc92e8144db0b1449c21018e
Murkrow <marcocoppola02.mc@gmail.com>
2024-09-22 18:49:16 +0200

    Haptic toast

--- lib/core/utils/toaster.dart
+++ lib/core/utils/toaster.dart
@@ -2,13 +2,14 @@ import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:haptic_feedback/haptic_feedback.dart';
 
 /// This class is used to show toast without context for messages/errors
 class Toaster {
   /*
   * Modify global config
   */
-  static Alignment _alignment = Alignment.topRight;
+  static Alignment _alignment = Alignment.topCenter;
 
   static void setAlignment(Alignment alignment) {
     _alignment = alignment;
@@ -17,11 +18,33 @@ class Toaster {
   static void show(String message, Color color, IconData icon) {
     BotToast.showCustomText(
       onlyOne: true,
+      wrapToastAnimation: (controller, cancel, child) {
+        // From top to bottom, from small to large
+        return FadeTransition(
+          opacity: CurvedAnimation(
+            parent: controller,
+            curve: Curves.easeOut,
+          ),
+          child: SlideTransition(
+            position: Tween<Offset>(
+              begin: Offset(0, -0.5), // Start above the widget's initial position
+              end: Offset(0, 0.2),      // End at its original position
+            ).animate(CurvedAnimation(
+              parent: controller,
+              curve: Curves.easeOut,
+            )),
+            child: child,
+          ),
+        );
+      },
+      // animationDuration: const Duration(seconds: 3),
+      // animationReverseDuration: const Duration(seconds: 3),
       duration: const Duration(seconds: 3),
+      align: _alignment,
       toastBuilder: (cancel) {
         return Container(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            margin: const EdgeInsets.only(bottom: 20),
+            //margin: const EdgeInsets.only(bottom: 20),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(15),
               color: color,
@@ -36,7 +59,10 @@ class Toaster {
                 const SizedBox(width: 10),
                 Text(
                   message,
-                  style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
                 ),
               ],
             ));
@@ -44,19 +70,31 @@ class Toaster {
     );
   }
 
-  static void success(String message) {
+  static void success(String message) async {
     show(message, Colors.green, CupertinoIcons.check_mark_circled);
+    await Haptics.canVibrate()
+        ? await Haptics.vibrate(HapticsType.success)
+        : null;
   }
 
-  static void error(String message) {
+  static void error(String message) async {
     show(message, Colors.red, CupertinoIcons.xmark_circle);
+    await Haptics.canVibrate()
+        ? await Haptics.vibrate(HapticsType.error)
+        : null;
   }
 
-  static void warning(String message) {
+  static void warning(String message) async {
     show(message, Colors.orange, CupertinoIcons.exclamationmark_triangle);
+    await Haptics.canVibrate()
+        ? await Haptics.vibrate(HapticsType.warning)
+        : null;
   }
 
-  static void info(String message) {
+  static void info(String message) async {
     show(message, Colors.blue, CupertinoIcons.info);
+    await Haptics.canVibrate()
+        ? await Haptics.vibrate(HapticsType.rigid)
+        : null;
   }
 }
