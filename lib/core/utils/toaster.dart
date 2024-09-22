718c718efd790e8896f9e06dbba572ecc1d46c60
Murkrow <marcocoppola02.mc@gmail.com>
2024-09-22 15:13:25 +0200

    Moved to boattoast from toastification

--- lib/core/utils/toaster.dart
+++ lib/core/utils/toaster.dart
@@ -1,32 +1,46 @@
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
-import 'package:toastification/toastification.dart';
 
 /// This class is used to show toast without context for messages/errors
 class Toaster {
-
-
   /*
   * Modify global config
   */
   static Alignment _alignment = Alignment.topRight;
+
   static void setAlignment(Alignment alignment) {
     _alignment = alignment;
   }
 
   static void show(String message, Color color, IconData icon) {
-    toastification.show(
-      title: Text(message),
-      description: null,
-      style: ToastificationStyle.flat,
-      icon: Icon(icon),
-      alignment: _alignment,
-      primaryColor: color,
-      showProgressBar: false,
-      //applyBlurEffect: true,
-      pauseOnHover: true,
-      autoCloseDuration: const Duration(seconds: 5),
+    BotToast.showCustomText(
+      onlyOne: true,
+      duration: const Duration(seconds: 3),
+      toastBuilder: (cancel) {
+        return Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            margin: const EdgeInsets.only(bottom: 20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: color,
+            ),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
+                  icon,
+                  color: Colors.white,
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  message,
+                  style: const TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ));
+      },
     );
   }
 
@@ -41,7 +55,7 @@ class Toaster {
   static void warning(String message) {
     show(message, Colors.orange, CupertinoIcons.exclamationmark_triangle);
   }
-  
+
   static void info(String message) {
     show(message, Colors.blue, CupertinoIcons.info);
   }
